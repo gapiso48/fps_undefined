f15b0652c1b4af35be0d5ba6f11c98f32cc81294
Gabriel <gaboqv@gmail.com>
2017-01-12 21:46:10 -0500

    un script basico basico para el jugadora que da funcionamiento a prefab  "flag"

---
+++ ScriptBandera/ToggleEndGame.cs
@@ -0,0 +1,15 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class ToggleEndGame : MonoBehaviour {
+
+    void OnCollisionEnter(Collision col)
+    {
+        if (col.gameObject.name == "flag")
+        {
+            print("hello");
+            col.gameObject.transform.parent = this.gameObject.transform;
+        }
+    }	
+}
